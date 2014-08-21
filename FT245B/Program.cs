269074f77c5d8375a5527e7ac3fdd1bc2e4a46f5
HRiecke <Riecke.heiko@googlemail.com>
2014-08-21 05:55:39 +0200

    add comments

--- FT245B/Program.cs
+++ FT245B/Program.cs
@@ -17,7 +17,7 @@ namespace FT245B
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
