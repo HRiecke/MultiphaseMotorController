e37bce6c847da9a5e9ab3f6b52dabafb0b00ac23
HRiecke <Riecke.heiko@googlemail.com>
2014-08-21 05:54:58 +0200

    add comments

--- FT245B/IStepMode.cs
+++ FT245B/IStepMode.cs
@@ -5,12 +5,31 @@ using System.Text;
 
 namespace FT245B
 {
+    /// <summary>
+    /// base interface for step mode implentaiones.
+    /// </summary>
     public interface IStepMode
     {
+        /// <summary>
+        /// Start motor stepping.
+        /// </summary>
         void Start();
+        /// <summary>
+        /// Stopt motor stepping.
+        /// </summary>
         void Stop();
+        /// <summary>
+        /// Get or Set hier interneal sleep time.
+        /// ~ control with this motor spead 
+        /// </summary>
         int Sleep { get; set; }
+        /// <summary>
+        /// Set hear CW or CCW.
+        /// </summary>
         bool Inverse { set; }
+        /// <summary>
+        /// Event hndler for internal log informations.
+        /// </summary>
         event FT245B.StepBase.LogHandler LogMsg;
     }
 }
